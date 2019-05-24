2390785393df37eb171149af2f6ceb7fb79be025
yijiaQ <576962583@qq.com>
2019-05-23 18:51:14 -0700

    first commitment

---
+++ client/src/reducers/streamReducer.js
@@ -0,0 +1,30 @@
+import _ from 'lodash';
+import {
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    DELETE_STREAM,
+    EDIT_STREAM
+} from '../actions/type';
+
+export default (state = {}, action) => {
+    switch (action.type) {
+        case FETCH_STREAMS:
+            // console.log({ ...state, ..._.mapKeys(action.payload, 'id') });
+            return { ...state, ..._.mapKeys(action.payload, 'id') };
+        case FETCH_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case CREATE_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case EDIT_STREAM:
+            console.log({ ...state, [action.payload.id]: action.payload });
+            return { ...state, [action.payload.id]: action.payload };
+        case DELETE_STREAM:
+            return _.omit(state, action.payload);
+        default:
+            return state;
+
+
+    }
+
+}
